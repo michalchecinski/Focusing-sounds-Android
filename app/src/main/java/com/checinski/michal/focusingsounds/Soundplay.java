e496dfebe023b9718aac125572134441d9ed2a4a
Michał Chęciński <michal.checinski@outlook.com>
2016-05-21 10:05:17 +0200

    first commit

---
+++ app/src/main/java/com/checinski/michal/focusingsounds/Soundplay.java
@@ -0,0 +1,13 @@
+package com.checinski.michal.focusingsounds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Soundplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soundplay);
+    }
+}
